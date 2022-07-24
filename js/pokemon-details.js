6600b3cd8a282bc33eb843ecd589d045b0d5086c
Luiz Preto <103388468+Luizth25@users.noreply.github.com>
2022-07-24 00:47:12 -0300

    Algumas atualizações feitas no código

--- js/pokemon-details.js
+++ js/pokemon-details.js
@@ -1,2 +1,25 @@
-import { pokeDetail } from "./main.js";
-document.getElementById("pokeInfo").click = pokeDetail();
+async function pokeDetail() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const pokeParam = urlParams.get("id");
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokeParam}`;
+  try {
+    const pokeResponse = await fetch(url);
+    const data = await pokeResponse.json();
+    const name = document.getElementById("name");
+    const id = document.getElementById("id");
+    const img = data.sprites.front_default;
+    document.getElementById("picture").setAttribute("src", img);
+    id.innerHTML = "#" + data.id;
+    name.innerHTML = data.name;
+    const move = data.moves;
+    const movi = move.map(
+      (moviment) => `<li><p>${moviment.move.name}</p></li>`
+    );
+    const movimento = movi.slice(0, 5).join("");
+    const ul = document.getElementById("moveList");
+    ul.innerHTML = movimento;
+  } catch (error) {
+    return error;
+  }
+}
+pokeDetail();
--- js/search-for-type.js
+++ js/search-for-type.js
@@ -1,3 +1,26 @@
-import { Home, searchForType } from "./main.js";
+import { Home } from "./main.js";
 document.querySelector(".Home").onclick = Home;
+
+async function searchForType() {
+  const pokemonsType = document.getElementById("pokemonsType").value;
+  const url = `https://pokeapi.co/api/v2/type/${pokemonsType}`;
+  try {
+    const pokemonType = await fetch(url);
+    const data = await pokemonType.json();
+    const list = data.pokemon;
+    list.forEach((pokeType) => {
+      const pokeArray = pokeType.pokemon.name;
+      const pokeUrl = pokeType.pokemon.url;
+      const pokeName = document.getElementById("pokeName");
+      const separateUrl = pokeUrl
+        .split("/")
+        .map((urlSeparate) => String(urlSeparate));
+      const idcaught = separateUrl.slice(6, 7);
+      pokeName.innerHTML += `<li><a id="pokeInfo" href="./pokemon-details.html?id=${idcaught}">#${idcaught} ${pokeArray}</a></li>`;
+    });
+  } catch (error) {
+    alert("Tipo não encontrado");
+    window.location.reload(true);
+  }
+}
 document.getElementById("searchForType").onclick = searchForType;
